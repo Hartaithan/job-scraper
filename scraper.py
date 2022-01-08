92070e34b1f6f422f090b3e99e808879465340eb
hartaithan <hartaithan@gmail.com>
2022-01-08 15:52:55 +0900

    scrape company name, salary and createdate

--- scraper.py
+++ scraper.py
@@ -5,5 +5,8 @@ url = "https://rabota.ykt.ru/jobs?categoriesIds=2083&page=1"
 result = BeautifulSoup(requests.get(url).text, "html.parser")
 
 vacancies = result.find_all(class_='r-vacancy_list_item')
-for each in vacancies:
-    print(each.find(class_='r-vacancy_title').get_text())
+for v in vacancies:
+    title = v.find(class_='r-vacancy_title').get_text()
+    company = v.find(class_='r-vacancy_company').find("a").get_text()
+    salary = v.find(class_='r-vacancy_salary').get_text()
+    createdate = v.find(class_='r-vacancy_createdate').get_text()

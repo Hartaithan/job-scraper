f085a9c830cf207ff4c2e7941db1006f8f38e18f
hartaithan <hartaithan@gmail.com>
2022-01-08 16:00:57 +0900

    scrape education, experience and schedule

--- scraper.py
+++ scraper.py
@@ -10,3 +10,7 @@ for v in vacancies:
     company = v.find(class_='r-vacancy_company').find("a").get_text()
     salary = v.find(class_='r-vacancy_salary').get_text()
     createdate = v.find(class_='r-vacancy_createdate').get_text()
+    box = v.find(class_='r-vacancy_box').find_all('dd')
+    education = box[0].get_text()
+    experience = box[1].get_text()
+    schedule = box[2].get_text()

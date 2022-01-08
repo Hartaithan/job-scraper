1e847d5c74a3860410e41c90175aa16542b20400
hartaithan <hartaithan@gmail.com>
2022-01-08 17:45:43 +0900

    put all results in one array

--- scraper.py
+++ scraper.py
@@ -2,13 +2,14 @@ from bs4 import BeautifulSoup, Comment
 import requests
 
 url = "https://rabota.ykt.ru/jobs?categoriesIds=2083&page=1"
-result = BeautifulSoup(requests.get(url).text, "html.parser")
+get_html = BeautifulSoup(requests.get(url).text, "html.parser")
 
-vacancies = result.find_all(class_='r-vacancy_list_item')
-for v in vacancies:
+result = list()
+vacancies = get_html.find_all(class_='r-vacancy_list_item')
+for index, v in enumerate(vacancies, start=1):
     title = v.find(class_='r-vacancy_title').get_text()
     company = v.find(class_='r-vacancy_company').find("a").get_text()
-    salary = v.find(class_='r-vacancy_salary').get_text()
+    salary = v.find(class_='r-vacancy_salary').get_text().replace('\n', '')
     createdate = v.find(class_='r-vacancy_createdate').get_text()
 
     box = v.find(class_='r-vacancy_box').find_all('dd')
@@ -16,8 +17,15 @@ for v in vacancies:
     experience = box[1].get_text()
     schedule = box[2].get_text()
 
-    full_titiles = v.find_all(class_='r-vacancy_body_full_title')
-    description = full_titiles[0].find_previous_sibling().get_text()
-    responsibilities = full_titiles[0].find_next_sibling().get_text()
-    requirements = full_titiles[1].find_next_sibling().get_text()
-    conditions = full_titiles[2].find_next_sibling().get_text()
+    full_titles = v.find_all(class_='r-vacancy_body_full_title')
+    description = full_titles[0].find_previous_sibling().get_text()
+    responsibilities = full_titles[0].find_next_sibling(
+    ).get_text().replace('\n', '')
+    requirements = full_titles[1].find_next_sibling(
+    ).get_text().replace('\n', '')
+    conditions = full_titles[2].find_next_sibling(
+    ).get_text().replace('\n', '')
+    result.append({"id": index, "title": title, "company": company, "salary": salary,
+                   "createdate": createdate, "education": education, "schedule": schedule, "description": description, "responsibilities": responsibilities, "requirements": requirements, "conditions": conditions})
+
+print(list(result))
